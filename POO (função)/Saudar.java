e12322949e2fb730a8e2772aa878acfb59e0cf6c
lari2806 <124395564+lari2806@users.noreply.github.com>
2024-02-22 15:19:25 -0300

    Add files via upload

---
+++ POO (função)/Saudar.java
@@ -0,0 +1,8 @@
+class Saudar{
+    public static void ola(String nome){
+        System.out.println("Olá " + nome);
+    }
+    public static void main(String[] args) {
+        ola("Kleber");
+    }
+}
---
+++ POO (função)/saudar.py
@@ -0,0 +1,5 @@
+def saudar(nome):
+        return print("Olá", nome,)
+nome = input('digite seu nome: ')
+men=saudar(nome)
+print(men)
