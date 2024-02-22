0ecfedfbe12a6b52cfaa2000a3ac9818c2b8cfc8
lari2806 <124395564+lari2806@users.noreply.github.com>
2024-02-22 15:30:56 -0300

    Update saudar.py

--- POO (função)/saudar.py
+++ POO (função)/saudar.py
@@ -1,5 +1,5 @@
-def saudar(nome):
-        return print("Olá", nome,)
-nome = input('digite seu nome: ')
-men=saudar(nome)
+def saudar(name):
+    return "Olá " + name
+name = input('digite seu nome: ')
+men=saudar(name)
 print(men)
