c198b378e81aee33660a13bc62ec55ab60686054
lari2806 <124395564+lari2806@users.noreply.github.com>
2024-02-23 11:18:02 -0300

    Delete POO (função)/Saudar.java

--- POO (função)/Saudar.java
+++
@@ -1,8 +0,0 @@
-class Saudar{
-    public static void ola(String nome){
-        System.out.println("Olá " + nome);
-    }
-    public static void main(String[] args) {
-        ola("Kleber");
-    }
-}
