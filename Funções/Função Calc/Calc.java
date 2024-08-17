7a79ac3bcf4444172c18979ba7e08c71c97bee0d
Larissa <larissa.rfs28@gmai.com>
2024-08-17 17:21:59 -0300

    Modificando arquivo.

--- Funções/Função Calc/Calc.java
+++ Funções/Função Calc/Calc.java
@@ -1,20 +1,23 @@
 class Calc {
+
     public static void main(String[] args){
-        int Escolha = (int)(Math.random()*4+1);
-        System.out.println("Escolha: \n 1 - Soma \n 2 - Subtração \n 3 - Multiplicação \n 4 - Divisão ");
-        System.out.println("Escolheu:\n" + Escolha);
+        int escolha = (int)(Math.random()*4+1);
+        System.out.println("escolha: \n 1 - Soma \n 2 - Subtração \n 3 - Multiplicação \n 4 - Divisão ");
+        System.out.println("Escolheu:\n" + escolha);
+
         int x = (int) (Math.random()*10+1);
         System.out.println("Primeiro Número:\n" + x);
+        
         int y = (int) (Math.random()*10+1);
         System.out.println("Segundo Número:\n" + y);
 
-        if(Escolha == 1){
+        if(escolha == 1){
         System.out.println("Resultado\n" + (x+y));
-        }else if(Escolha == 2){
+        }else if(escolha == 2){
         System.out.println("Resultado\n"+ (x-y));
-        }else if (Escolha == 3){
+        }else if (escolha == 3){
         System.out.println("Resultado\n"+ (x*y));
-        }else if (Escolha == 4){
+        }else if (escolha == 4){
          System.out.println("Resultado\n"+ (x/y));
         }else{
         System.out.println("opcão inválida");
