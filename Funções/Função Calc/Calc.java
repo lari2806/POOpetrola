4effa0110222916830c12b40668eb9d9a61effae
Larissa <124395564+lari2806@users.noreply.github.com>
2024-08-31 20:39:03 -0300

    Modificando arquivo java.

--- Funções/Função Calc/Calc.java
+++ Funções/Função Calc/Calc.java
@@ -5,20 +5,20 @@ class Calc {
         System.out.println("escolha: \n 1 - Soma \n 2 - Subtração \n 3 - Multiplicação \n 4 - Divisão ");
         System.out.println("Escolheu:\n" + escolha);
 
-        int x = (int) (Math.random()*10+1);
-        System.out.println("Primeiro Número:\n" + x);
+        int primeiroNumero = (int) (Math.random()*10+1);
+        System.out.println("Primeiro Número:\n" + primeiroNumero);
         
-        int y = (int) (Math.random()*10+1);
+        int segundoNumero = (int) (Math.random()*10+1);
         System.out.println("Segundo Número:\n" + y);
 
         if(escolha == 1){
-        System.out.println("Resultado\n" + (x+y));
+        System.out.println("Resultado\n" + (primeiroNumero+segundoNumero));
         }else if(escolha == 2){
-        System.out.println("Resultado\n"+ (x-y));
+        System.out.println("Resultado\n"+ (primeiroNumero-segundoNumero));
         }else if (escolha == 3){
-        System.out.println("Resultado\n"+ (x*y));
+        System.out.println("Resultado\n"+ (primeiroNumero*segundoNumero));
         }else if (escolha == 4){
-         System.out.println("Resultado\n"+ (x/y));
+         System.out.println("Resultado\n"+ (primeiroNumero/segundoNumero));
         }else{
         System.out.println("opcão inválida");
         }
--- Funções/Função Horário/Horário.java
+++ Funções/Função Horário/Horário.java
@@ -7,10 +7,10 @@ class Hora{
     }
 
     public static void saudação(){
-        var hora_A =LocalTime.now();
-        var horaf = hora_A.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
-        System.out.println("o horário é: " + horaf);
-        int hora = hora_A.getHour();
+        var horaAtual =LocalTime.now();
+        var horaFormatada = horaAtual.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+        System.out.println("o horário é: " + horaFormatada);
+        int hora = horaAtual.getHour();
          if (hora >= 5 && hora <= 11){
             System.out.println( "Bom Dia!");
         } else if(hora >=12 && hora <=18){
